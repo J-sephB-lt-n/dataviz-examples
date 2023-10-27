21dc6df0a9fa237cbe0e52f8d821c3980df34f8a
J-sephB-lt-n <joseph.jazz.bolton@gmail.com>
2023-10-27 09:11:26 +0200

    feat(example_finder.py): built basic example finder functionality
    BREAKING CHANGE:

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # dataviz-examples
-A searchable database of code examples for common data visualizations in multiple coding languages
+
+A searchable database of code examples for common data visualizations in multiple coding languages.
+
+I built this package because I like to quickly try different data visualizations without having to leave my IDE, and also because I don't like doing the same task more than once. 
+
+```python
+>>> example_finder = ExampleFinder()
+>>> example_finder.search_by_tags("colour by sign")
+2  Line Plot Coloured by Sign  [python]  {'matplotlib'}
+>>> example_finder.print_example_metadata(2)
+PLOT_NAME      Line Plot Coloured by Sign
+PLOT_ALIASES   {'Line Plot Coloured by Threshold'}
+DESCRIPTION    Line plot which changes colour based on which side of 0 the value falls on 
+    (also includes transparent bar chart underneath)
+TAGS           {'dynamic', 'sign', 'colour', 'line', 'threshold'}
+FRAMEWORKS     {'matplotlib'}
+LANGUAGE       python
+>>> print( example_finder.get_example_code(2) )
+< prints out code to generate this plot >
+>>> exec( example_finder.get_example_code(2) )
+```
+![](./docs/line_chart_coloured_by_sign.png)
--- dataviz_examples/example_finder.py
+++ dataviz_examples/example_finder.py
@@ -8,9 +8,23 @@ from typing import Any
 
 class ExampleFinder:
     """
-    example_finder = ExampleFinder()
-    for example in example_finder.all_examples:
-        print(example)
+    Example Usage
+    -------------
+    >>> example_finder = ExampleFinder()
+    >>> example_finder.search_by_tags("colour by sign")
+    2  Line Plot Coloured by Sign  [python]  {'matplotlib'}
+    >>> example_finder.print_example_metadata(2)
+    PLOT_NAME      Line Plot Coloured by Sign
+    PLOT_ALIASES   {'Line Plot Coloured by Threshold'}
+    DESCRIPTION    Line plot which changes colour based on sign
+        (also includes transparent bar chart underneath)
+    TAGS           {'dynamic', 'sign', 'colour', 'line', 'threshold'}
+    FRAMEWORKS     {'matplotlib'}
+    LANGUAGE       python
+    >>> print( example_finder.get_example_code(2) )
+    ...
+    >>> exec( example_finder.get_example_code(2) )
+    ...
     """
 
     def __init__(self) -> None:
@@ -36,6 +50,46 @@ class ExampleFinder:
                     example["code"] = file.read()
                 self.all_examples.append(example)
 
-    def search(self) -> list[Any]:
+    def search_by_tags(self, search_str: str) -> None:
         """docstring TODO"""
-        return ["still", "to", "do"]
+        search_words: set[str] = set(search_str.lower().split())
+        matches_found: dict[str, dict[str, Any]] = {}
+        for example_idx, example in enumerate(self.all_examples):
+            tags_matched: set[str] = example["tags"].intersection(search_words)
+            if len(tags_matched) > 0:
+                matches_found[example["plot_name"]] = {
+                    "index": example_idx,
+                    "n_tags_matched": len(tags_matched),
+                    "language": example["language"],
+                    "frameworks": example["frameworks"],
+                }
+
+        # sort search results by number of matched tags #
+        matches_found = dict(
+            sorted(
+                matches_found.items(),
+                key=lambda x: x[1]["n_tags_matched"],
+                reverse=True,
+            )
+        )
+        if len(matches_found) == 0:
+            print("< no examples found >")
+        else:
+            for example_name, example_info in matches_found.items():
+                print(
+                    f'{example_info["index"]}  {example_name}  [{example_info["language"]}]  {example_info["frameworks"]}'  # pylint: disable=line-too-long
+                )
+
+    def print_example_metadata(self, example_idx: int) -> None:
+        """docstring TODO"""
+        if example_idx < 0 or example_idx > len(self.all_examples) - 1:
+            raise NameError(f"example index {example_idx} does not exist")
+        for key, value in self.all_examples[example_idx].items():
+            if key != "code":
+                print(f"{key.upper():15}{value}")
+
+    def get_example_code(self, example_idx: int) -> str:
+        """docstring TODO"""
+        if example_idx < 0 or example_idx > len(self.all_examples) - 1:
+            raise NameError(f"example index {example_idx} does not exist")
+        return self.all_examples[example_idx]["code"]
